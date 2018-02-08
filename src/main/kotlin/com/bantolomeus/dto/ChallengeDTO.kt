bf008a12e03f81394cc77d307d3b1a6bd50c23ff
chrb <chrb@spreadshirt.net>
2018-02-08 17:35:26 +0100

    #27 created default challenge file

--- src/main/kotlin/com/bantolomeus/dto/ChallengeDTO.kt
+++ src/main/kotlin/com/bantolomeus/dto/ChallengeDTO.kt
@@ -2,7 +2,7 @@ package com.bantolomeus.dto
 
 data class ChallengeDTO(
         val pagesPerDay: Long = 0,
-        val pagesPlus: Long = 0,
+        val pagesSurplus: Long = 0,
         val startPagesPlus: Long = 0,
         var pagesEverRead: Long = 0,
         val dateStarted: String? = ""
