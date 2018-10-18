94414790664b0d05af74aa21a412ad7d28b0eed8
bens <bens@spreadshirt.net>
2018-10-18 14:11:54 +0200

    #63 UI: prettier to have consistent code style

--- client/app.js
+++ client/app.js
@@ -1,26 +1,19 @@
-// todo remove axios
-// todo remove npm warnings
-let axios = require('../node_modules/axios/dist/axios.min.js');
-
-'use strict';
-angular.module('breaker', [])
-    .controller('home', HomeController);
+angular.module('breaker', []).controller('home', HomeController);
 
 HomeController.$inject = ['$scope'];
 function HomeController($scope) {
+  activate();
+  function activate() {
+    let uri = '/challenge';
+    axios.get(uri).then(response => {
+      console.log('request received');
+      console.log(response);
+      $scope.response = response.data;
+      $scope.$apply();
+    });
+  }
 
-    activate();
-    function activate() {
-        let uri = '/challenge';
-        axios.get(uri).then((response) => {
-            console.log('request received');
-            console.log(response);
-            $scope.response = response.data;
-            $scope.$apply();
-        });
-    }
-
-    $scope.square = () => {
-        $scope.squareOutput = $scope.squareInput * $scope.squareInput;
-    }
+  $scope.square = () => {
+    $scope.squareOutput = $scope.squareInput * $scope.squareInput;
+  };
 }
