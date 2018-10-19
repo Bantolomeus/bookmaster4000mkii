8c031586290debfbe66cae280253ebf4e73fb162
bens <bens@spreadshirt.net>
2018-10-19 08:57:56 +0200

    #54 UI: shape of red dynamic based on pagesAheadOfPlan (less -> red++)

--- client/app.js
+++ client/app.js
@@ -6,4 +6,13 @@ function HomeController($scope, $resource) {
     function activate() {
         $scope.progress = $resource('/challenge').get(); // naming is supposed to match (issue #66)
     }
+
+    $scope.redRgbForNegativeValue = function(base) {
+        if (base < 0) {
+            let value = 205 / ((base * -1) / 10);
+            return `rgb(255, ${value}, ${value})`;
+        } else {
+            return 'rgb(223, 240, 216)';
+        }
+    };
 }
