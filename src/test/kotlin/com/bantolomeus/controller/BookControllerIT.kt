23ddb09b1e4118e2856f23b7a6e9e4c17fd2c39d
Christian Bantel <christian.bantel@dbfuhrpark.com>
2018-10-27 21:40:13 +0200

    #61 Extract initialization of classes before each test

--- src/test/kotlin/com/bantolomeus/controller/BookControllerIT.kt
+++ src/test/kotlin/com/bantolomeus/controller/BookControllerIT.kt
@@ -9,6 +9,7 @@ import com.bantolomeus.repository.BookUpdatesRepository
 import com.bantolomeus.repository.ChallengeRepository
 import com.bantolomeus.service.BookService
 import com.bantolomeus.service.ChallengeService
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.test.context.junit4.SpringRunner
@@ -21,20 +22,20 @@ import kotlin.test.assertTrue
 @RunWith(SpringRunner::class)
 class BookControllerIT {
 
+    private lateinit var today: String
+    private lateinit var fileNameChallenge: String
+    private lateinit var challengeRepository: ChallengeRepository
+    private lateinit var challengeService:ChallengeService
+
+    private lateinit var fileNameBook: String
+    private lateinit var bookRepository: BookRepository
+    private lateinit var fileNameBookUpdates: String
+    private lateinit var bookUpdateRepository: BookUpdatesRepository
+    private lateinit var bookService: BookService
+    private lateinit var bookController: BookController
+
     @Test
     fun createBookWithCurrentPage() {
-        val today = dateFormat.format(Date())
-        val fileNameChallenge = "testChallenge.json"
-        val challengeRepository = ChallengeRepository(fileNameChallenge)
-        val challengeService = ChallengeService(challengeRepository)
-
-        val fileNameBook = "testBooks.json"
-        val bookRepository = BookRepository(fileNameBook)
-        val fileNameBookUpdates = "testBookUpdates.json"
-        val bookUpdateRepository = BookUpdatesRepository(fileNameBookUpdates)
-        val bookService = BookService(bookRepository, bookUpdateRepository, challengeService)
-        val bookController = BookController(bookService)
-
         val bookDTO = BookDTO(name = "testBook", author = "Jim Carry", pagesTotal = 314, currentPage = 12)
         val challengeDTO = ChallengeDTO(pagesPerDay = 15, pagesAheadOfPlan = 0,
                 startPagesAheadOfPlan = 0, pagesSinceStart = 0, pagesEverRead = 1000,
@@ -53,5 +54,25 @@ class BookControllerIT {
         File(fileNameBook).deleteRecursively()
         File(fileNameBookUpdates).deleteRecursively()
         File(fileNameChallenge).deleteRecursively()
+    }
+
+    @Test
+    fun bookUpdatesIsSortedByDateDESC() {
+
+    }
+
+    @Before
+    fun setUpClasses() {
+        today = dateFormat.format(Date())
+        fileNameChallenge = "testChallenge.json"
+        challengeRepository = ChallengeRepository(fileNameChallenge)
+        challengeService = ChallengeService(challengeRepository)
+
+        fileNameBook = "testBooks.json"
+        bookRepository = BookRepository(fileNameBook)
+        fileNameBookUpdates = "testBookUpdates.json"
+        bookUpdateRepository = BookUpdatesRepository(fileNameBookUpdates)
+        bookService = BookService(bookRepository, bookUpdateRepository, challengeService)
+        bookController = BookController(bookService)
     }
 }
