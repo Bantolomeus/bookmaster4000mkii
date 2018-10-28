c613184d326b3a79df896029c6bb85f95cf2931f
Christian Bantel <christian.bantel@dbfuhrpark.com>
2018-10-28 11:07:35 +0100

    #61 Extend create book test about update

--- src/test/kotlin/com/bantolomeus/controller/BookControllerIT.kt
+++ src/test/kotlin/com/bantolomeus/controller/BookControllerIT.kt
@@ -53,13 +53,12 @@ class BookControllerIT {
     }
 
     @Test
-    fun createBookWithUpdateAndBookUpdatesAreSortedByDateDESC() {
+    fun createAndUpdateBookWithUpdateAndBookUpdatesAreSortedByDateDESC() {
         val bookDTO = BookDTO(name = "testBook", author = "Jim Carry", pagesTotal = 314, currentPage = 12)
         val bookUpdateDTO = BookUpdateInputDTO(27)
         val challengeDTO = ChallengeDTO(pagesPerDay = 15, pagesAheadOfPlan = 0,
                 startPagesAheadOfPlan = 0, pagesSinceStart = 0, pagesEverRead = 1000,
                 dateStarted = dateFormat.format(GregorianCalendar(2018, 4, 22).time))
-        val bookUpdate = BookUpdateOutputDTO(name = "testBook", pagesRead = 12, date = today)
         val bookUpdates = BookUpdatesFileDTO(mutableListOf(
                 BookUpdateOutputDTO("Bugs Bunny", 152, "13/06/2018"),
                 BookUpdateOutputDTO("Fire", 2, "13/06/2018"),
@@ -75,8 +74,8 @@ class BookControllerIT {
 
         assertNotEquals(challengeInit.pagesAheadOfPlan, challengeUpdated.pagesAheadOfPlan)
         assertEquals(challengeUpdated.pagesEverRead, (challengeInit.pagesEverRead + bookResponse.currentPage))
-        assertTrue { bookUpdatesUpdated.bookUpdates.contains(bookUpdate) }
-        assertEquals(bookUpdatesUpdated.bookUpdates[0].name, "testBook")
+        assertEquals(bookUpdatesUpdated.bookUpdates[0].name, bookDTO.name)
+        assertEquals(bookUpdatesUpdated.bookUpdates[0].pagesRead, bookDTO.currentPage)
         assertEquals(bookUpdatesUpdated.bookUpdates[1].name, bookUpdates.bookUpdates[0].name)
         assertEquals(bookUpdatesUpdated.bookUpdates[2].name, bookUpdates.bookUpdates[1].name)
         assertEquals(bookUpdatesUpdated.bookUpdates[3].name, bookUpdates.bookUpdates[2].name)
@@ -84,11 +83,16 @@ class BookControllerIT {
         assertEquals(bookUpdatesUpdated.bookUpdates[5].name, bookUpdates.bookUpdates[4].name)
 
         val updateResponse = bookController.updateBook(bookUpdateDTO, bookDTO.name)
-
         val challengeUpdated2 = challengeRepository.getChallenge()
 
         assertEquals(challengeUpdated2.pagesEverRead, (challengeInit.pagesEverRead + bookUpdateDTO.currentPage))
-
+        assertEquals(updateResponse.bookUpdates[0].name, bookDTO.name)
+        assertEquals(updateResponse.bookUpdates[0].pagesRead, bookUpdateDTO.currentPage)
+        assertEquals(updateResponse.bookUpdates[1].name, bookUpdates.bookUpdates[0].name)
+        assertEquals(updateResponse.bookUpdates[2].name, bookUpdates.bookUpdates[1].name)
+        assertEquals(updateResponse.bookUpdates[3].name, bookUpdates.bookUpdates[2].name)
+        assertEquals(updateResponse.bookUpdates[4].name, bookUpdates.bookUpdates[3].name)
+        assertEquals(updateResponse.bookUpdates[5].name, bookUpdates.bookUpdates[4].name)
     }
 
 //    @Test
