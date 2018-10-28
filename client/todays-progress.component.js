ce4543c8056bdfbf64b2d9e0079369aebdd45620
Husterknupp <husterknupp@outlook.com>
2018-10-28 21:15:55 +0100

    #71 colored book cards

--- client/todays-progress.component.js
+++ client/todays-progress.component.js
@@ -4,7 +4,25 @@ module.exports.default = angular.module('todaysProgress', []).component('todaysP
     template: require('./todays-progress.component.html')
 }).name;
 
-TodaysProgress.$inject = [];
-function TodaysProgress() {
-    console.log('todaysProgress');
+TodaysProgress.$inject = ['$resource'];
+function TodaysProgress($resource) {
+    let ctrl = this;
+
+    ctrl.$onInit = () => {
+        ctrl.books = $resource('/books').query();
+    };
+
+    ctrl.nameToHexColor = name => {
+        let hash = 0;
+        for (let i = 0; i < name.length; i++) {
+            hash = name.charCodeAt(i) + ((hash << 5) - hash);
+        }
+        let color = '#';
+        for (let i = 0; i < 3; i++) {
+            let value = (hash >> (i * 8)) & 0xFF;
+            color += (`00${value.toString(16)}`).substr(-2);
+        }
+        let transparency10Percent = `33`; // cudos to @lopspower https://gist.github.com/lopspower/03fb1cc0ac9f32ef38f4
+        return `${color}${transparency10Percent}`;
+    }
 }
