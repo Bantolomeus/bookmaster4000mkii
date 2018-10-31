0c922efea012eb5ba0c069027832a3f94ae0653a
Husterknupp <husterknupp@outlook.com>
2018-10-31 22:28:40 +0100

    #71 UI: again renaming..

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import overallProgressComponent from './overall-progress.component';
 import todaysProgressComponent from './todays-progress.component';
 import navBarComponent from './nav-bar.component';
-import routes from './routes.js';
+import routesConfig from './routes.config';
 
 angular
   .module('bookmaster', [
@@ -11,4 +11,4 @@ angular
     todaysProgressComponent.default,
     navBarComponent.default
   ])
-  .config(routes.default);
+  .config(routesConfig.default);
