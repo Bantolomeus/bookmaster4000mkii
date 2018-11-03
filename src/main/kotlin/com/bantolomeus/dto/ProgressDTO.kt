5f717ef9b4e70c58539be4051276918948829f42
Christian Bantel <christian.bantel@dbfuhrpark.com>
2018-11-03 20:42:15 +0100

    #66 First step for progress handling

--- src/main/kotlin/com/bantolomeus/dto/ProgressDTO.kt
+++ src/main/kotlin/com/bantolomeus/dto/ProgressDTO.kt
@@ -1,3 +1,7 @@
 package com.bantolomeus.dto
 
-data class ProgressUpdateDTO(val date: String = "", val pagesRead: Long = 0)
+data class ProgressUpdateDTO(val date: String = "",
+                             val pagesRead: Long = 0)
+
+data class ProgressFileDTO(var pagesSinceStart: Long = 0,
+                           var pagesEverRead: Long = 0)
