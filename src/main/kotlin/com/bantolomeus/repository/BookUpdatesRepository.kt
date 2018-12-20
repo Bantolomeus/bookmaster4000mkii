6e30caf25522c485a7011e6d6c24700a123198fb
Christian Bantel <christian.bantel@dbfuhrpark.com>
2018-12-20 19:51:47 +0100

    #99 Convert Husterknupps comments into code

--- src/main/kotlin/com/bantolomeus/repository/BookUpdatesRepository.kt
+++ src/main/kotlin/com/bantolomeus/repository/BookUpdatesRepository.kt
@@ -1,9 +1,14 @@
 package com.bantolomeus.repository
 
+import com.bantolomeus.date.dateFormat
+import com.bantolomeus.dto.BookDTO
+import com.bantolomeus.dto.BookUpdateOutputDTO
 import com.bantolomeus.dto.BookUpdatesFileDTO
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.stereotype.Repository
 import java.io.File
+import java.util.*
 
 const val BOOK_UPDATES_FILE = "bookUpdates.json"
 
@@ -41,5 +46,9 @@ class BookUpdatesRepository(private val bookUpdatesFile: String = BOOK_UPDATES_F
         } catch (exception: Exception) {
             BookUpdatesFileDTO()
         }
+    }
+
+    fun getUpdateFromToday(bookName: String): BookUpdateOutputDTO? {
+        return getBookUpdates()?.bookUpdates?.firstOrNull { it.date == dateFormat.format(Date()) && it.name == bookName }
     }
 }
--- src/main/kotlin/com/bantolomeus/service/BookService.kt
+++ src/main/kotlin/com/bantolomeus/service/BookService.kt
@@ -24,29 +24,24 @@ class BookService(private val bookRepository: BookRepository,
 
     fun updateBook(bookUpdate: BookUpdateInputDTO, bookName: String): BookUpdatesFileDTO {
         val book = bookRepository.getBookByName(bookName) ?: return BookUpdatesFileDTO()
-
         if (book.currentPage == book.pagesTotal) {
-            book.readTime = ((Date().time - dateFormat.parse(book.dateStarted).time) / DIVISOR_FOR_DAY)
+            book.readTime = ((Date().time.minus(dateFormat.parse(book.dateStarted).time)) / DIVISOR_FOR_DAY)
         }
-        val oldPage = book.currentPage
-        book.currentPage = bookUpdate.currentPage
+
+        val oldPage = book.currentPage.also { book.currentPage = bookUpdate.currentPage}
         val pagesRead = bookUpdate.currentPage.minus(oldPage)
         if (pagesRead <= 0 || bookUpdate.currentPage > book.pagesTotal) {
             return BookUpdatesFileDTO()
         }
-
         bookRepository.updateBook(book)
 
-        // todo '.firstOrNull { it.date == currentDate && it.name == bookName }' is also something that should happen in the repo.
-        //      repository.getTodaysUpdatesForBook(foundBook)
-        // todo move exception into repo
-        //      in this case I don't see any value in running the bookmaster w/o a working BookUpdatesRepository
-        val bookUpdates = bookUpdatesRepository.getBookUpdates()
-                ?: throw IllegalStateException("BookUpdatesRepository not available but it should be")
         val currentDate = dateFormat.format(Date())
-        bookUpdates.bookUpdates.firstOrNull { it.date == currentDate && it.name == bookName }?.let {
-            return editUpdateFromToday(bookUpdates, bookName, pagesRead, currentDate, it, bookUpdate, oldPage)
-        } ?: return editUpdateFromToday(bookUpdates, bookName, pagesRead, bookUpdate.date ?: currentDate, BookUpdateOutputDTO(), bookUpdate, oldPage)
+        val updateFromToday = bookUpdatesRepository.getUpdateFromToday(book.name)
+        return if (updateFromToday == null) {
+            saveBookUpdate(bookName, pagesRead, bookUpdate.date ?: currentDate, BookUpdateOutputDTO(), bookUpdate, oldPage)
+        } else {
+            saveBookUpdate(bookName, pagesRead, currentDate, updateFromToday, bookUpdate, oldPage)
+        }
     }
 
     fun getBookWithUpdates(bookName: String): BookGetDTO {
@@ -72,16 +67,15 @@ class BookService(private val bookRepository: BookRepository,
         progressService.saveProgress(bookDTO.currentPage)
     }
 
-    private fun editUpdateFromToday(bookUpdates: BookUpdatesFileDTO?, bookName: String, pagesRead: Long,
-                                    currentDate: String, bookUpdateFromToday: BookUpdateOutputDTO,
-                                    bookUpdate: BookUpdateInputDTO, oldPage: Long): BookUpdatesFileDTO {
-        val oldBookUpdates = bookUpdates?.bookUpdates?.filter {
-            it.date != bookUpdateFromToday.date || it.name != bookName
+    private fun saveBookUpdate(bookName: String, pagesRead: Long, date: String, bookUpdateFromToday: BookUpdateOutputDTO?,
+                               bookUpdate: BookUpdateInputDTO, oldPage: Long): BookUpdatesFileDTO {
+        val oldBookUpdates = bookUpdatesRepository.getBookUpdates()?.bookUpdates?.filter {
+            it.date != bookUpdateFromToday?.date || it.name != bookName
         }?.toMutableList()
         oldBookUpdates?.add(0, BookUpdateOutputDTO(
                 name = bookName,
-                pagesRead = bookUpdate.currentPage.minus(oldPage).plus(bookUpdateFromToday.pagesRead),
-                date = currentDate)
+                pagesRead = bookUpdate.currentPage.minus(oldPage).plus(bookUpdateFromToday!!.pagesRead),
+                date = date)
         )
         progressService.saveProgress(pagesRead)
         return bookUpdatesRepository.saveBookUpdate(BookUpdatesFileDTO(oldBookUpdates ?: mutableListOf()))
